e094484bb73e4d9c2e716f451d22e738f2ce1510
McKay <kg89szeged@gmail.com>
2023-08-15 21:37:36 +0200

    [ UPDATE ] Technical Percent & Professional Percent counters was added

--- js/main.js
+++ js/main.js
@@ -38,3 +38,90 @@ let changeText = () => {
 
 changeText();
 setInterval(changeText, 3000);
+
+
+/* Professional Skills */
+
+const circle = document.querySelectorAll('.circle');
+
+circle.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots * marked / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked');
+    }
+})
+
+/* Technical Skill Counters */
+const htmlSkill         = document.getElementById('html-percent');
+const cssSkill          = document.getElementById('css-percent');
+const javascriptSkill   = document.getElementById('javascript-percent');
+const csharpSkill       = document.getElementById('csharp-percent');
+const sqlSkill          = document.getElementById('sql-percent');
+const designSkill       = document.getElementById('design-percent');
+
+const PercentCounter = async (percent, duration, skill) => {
+    const interval = 40;
+    const steps = duration / interval;
+    const increment = (percent - parseInt(skill.innerText)) / steps;
+
+    const timer = setInterval(async () => {
+        const currentPercent = parseInt(skill.innerText);
+        if (currentPercent < percent) {
+            const newPercent = currentPercent + increment;
+            skill.innerText = "" + Math.round(newPercent) + "%";
+        } else {
+            clearInterval(timer);
+        }
+    }, interval);
+}
+
+PercentCounter(95, 2000, htmlSkill);
+PercentCounter(92, 2000, cssSkill);
+PercentCounter(76, 2000, javascriptSkill);
+PercentCounter(80, 2000, csharpSkill);
+PercentCounter(64, 2000, sqlSkill);
+PercentCounter(82, 2000, designSkill);
+
+/* Professional Skill Counters */
+const skill1Percent = document.getElementById('skill1-percent');
+const skill2Percent = document.getElementById('skill2-percent');
+const skill3Percent = document.getElementById('skill3-percent');
+const skill4Percent = document.getElementById('skill4-percent');
+const skill5Percent = document.getElementById('skill5-percent');
+const skill6Percent = document.getElementById('skill6-percent');
+
+const PercentCounter2 = async (percent, skill) => {
+    const interval = 40;
+    const duration = (percent / interval) * 1000;
+    const steps = duration / interval;
+    const increment = (percent - parseInt(skill.innerText)) / steps;
+
+    const timer = setInterval(async () => {
+        const currentPercent = parseInt(skill.innerText);
+        if (currentPercent < percent) {
+            const newPercent = currentPercent + increment;
+            skill.innerText = "" + Math.round(newPercent) + "%";
+        } else {
+            clearInterval(timer);
+        }
+    }, interval);
+}
+
+PercentCounter2(90, skill1Percent);
+PercentCounter2(87, skill2Percent);
+PercentCounter2(88, skill3Percent);
+PercentCounter2(95, skill4Percent);
+PercentCounter2(91, skill5Percent);
+PercentCounter2(98, skill6Percent);
